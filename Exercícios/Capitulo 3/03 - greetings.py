549e8839e457e2d1ae401eeb300b1c0aa3ce1523
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-04-22 22:50:31 -0300

    Exercises list chapter 3

---
+++ Exercícios/Capitulo 3/03 - greetings.py
@@ -0,0 +1,7 @@
+names = ['Liz','Rodrigo', 'Luana', 'Thais', 'Yuri', 'Monica']
+
+print(f"bem vindo {names[0]}")
+print(f"bem vindo {names[1]}")
+print(f"bem vindo {names[2]}")
+print(f"bem vindo {names[3]}")
+print(f"bem vindo {names[4]}")
---
+++ Exercícios/Capitulo 3/04 - my_own_list.py
@@ -0,0 +1,5 @@
+transport_media = ['plane', 'bicycle', 'car']
+
+print(f"I would like to own a {transport_media[0]}")
+print(f"I would like to own a Caloi {transport_media[2]}")
+print(f"I would like to own a Honda {transport_media[2]}")
