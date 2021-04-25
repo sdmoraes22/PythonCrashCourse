560e8cc4e9c37489052b6809e9d7e3f583030bf3
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-04-25 19:16:15 -0300

    Sorting and reversing a pytnon list

---
+++ Exercícios/Capitulo 3/cars.py
@@ -0,0 +1,22 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort()
+# print(cars)
+
+# cars.sort(reverse=True)
+# print(cars)
+
+# print("\nHere is the original list:")
+# print(cars)
+
+# print("\nHere is the sorted list:")
+# print(sorted(cars))
+
+# print("\nHere is the original list again:")
+# print(cars)
+
+# print("\nHere is the original list reverse sorted:")
+# print(sorted(cars, reverse=True))
+
+print(cars)
+cars.reverse()
+print(cars)
