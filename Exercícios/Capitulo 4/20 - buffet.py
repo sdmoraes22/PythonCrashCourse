df0c21f41d6d320868caf69208ad7eba509ecba6
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-05-02 15:33:37 -0300

    Ending exercise chapter 4

---
+++ Exercícios/Capitulo 4/20 - buffet.py
@@ -0,0 +1,13 @@
+restaurant_food = ('rice', 'beans', 'steak', 'chicken', 'french fries')
+
+print("Menu")
+for food in restaurant_food:
+    print(food)
+
+# restaurant_food[0] = 'ice cream'
+
+restaurant_food = ('rice', 'beans', 'milk shake', 'ice cream', 'french fries')
+
+print("\nNew Menu")
+for food in restaurant_food:
+    print(food)
