2d1917d442286be3db30f4ae57bb3ffc8908cd69
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-05-07 12:54:13 -0300

    If else if else examples

---
+++ Exercícios/Capitulo 5/06 - voting.py
@@ -0,0 +1,7 @@
+age = 17
+if age >= 18:
+    print("You are old enougth to vote")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register as soon as you turn 18!")
