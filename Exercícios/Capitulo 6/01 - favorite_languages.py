700a429a5fe8db9027977edc3b0d4252bbc0202b
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-10-17 02:40:48 -0300

    Python dictionary exercise

---
+++ Exercícios/Capitulo 6/01 - favorite_languages.py
@@ -0,0 +1,10 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+language = favorite_languages['sarah'].title()
+
+print(f"Sarah's favorite language is {language}.")
---
+++ Exercícios/Capitulo 6/03 - person.py
@@ -0,0 +1,8 @@
+person = {
+    'first_name': 'foo',
+    'last_name': 'fool',
+    'age': 20,
+    'city': 'Rio de Janeiro'
+}
+
+print(f"Name {person['first_name']} {person['last_name']}, Age {person['age']}, City {person['city']}")
