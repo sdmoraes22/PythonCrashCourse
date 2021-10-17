5807d9e580261617ee9d562d1b1885b7fac641db
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-10-17 11:52:09 -0300

    Beggining of the 2nd exercise list

--- Exercícios/Capitulo 6/01 - favorite_languages.py
+++ Exercícios/Capitulo 6/01 - favorite_languages.py
@@ -1,10 +1,43 @@
 favorite_languages = {
     'jen': 'python',
     'sarah': 'c',
+    'chris': 'c',
     'edward': 'ruby',
     'phil': 'python'
 }
 
-language = favorite_languages['sarah'].title()
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}")
 
-print(f"Sarah's favorite language is {language}.")
+# Os dois loops abaixo tem o mesmo comportamento sendo de escolha do desnvolvedor
+# deixar o método keys() explícito ou não
+for name in favorite_languages.keys():
+    print(name.title())
+
+for name in favorite_languages:
+    print(name.title())
+
+friends = ['phil', 'sarah']
+
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"{name.title()}, I see you love {language}")
+
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take your pool")
+
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you for taking the pool")
+
+print("The folowing languages have been mentioned:")
+
+for language in favorite_languages.values():
+    print(language.title())
+
+for language in set(favorite_languages.values()):
+    print(language.title())
+
--- Exercícios/Capitulo 6/05 - glossary.py
+++ Exercícios/Capitulo 6/05 - glossary.py
@@ -3,11 +3,11 @@ programming_words = {
     'Program': 'A computer program is termed as an organized collection of instructions, which when executed perform a specific task or function.',
     'API': 'Application Programming Interface (API) is a set of rules, routines, and protocols to build software applications.',
     'Argument': 'Argument or arg is a value that is passed into a command or a function.',
-    'Bug': 'A bug is a general term used to denote an unexpected error or defect in hardware or software, which causes it to malfunction.'
+    'Bug': 'A bug is a general term used to denote an unexpected error or defect in hardware or software, which causes it to malfunction.',
+    'Objects': 'An object is a combination of related variables, constants and other data structures which can be selected and manipulated together.',
+    'Class': 'In Object-Oriented programming, a class refers to a set of related objects with common properties. Classes and the ability to create new classes render OOP a powerful and flexible programming model.',
+    'Code': 'Code or source code is a term used to describe a written set of instructions, written using the protocols of a particular language, such as Java, C or Python. '
 }
 
-print(f"Algorithm \n{programming_words['Algorithm']}\n")
-print(f"Program \n{programming_words['Program']}\n")
-print(f"API \n{programming_words['API']}\n")
-print(f"Argument \n{programming_words['Argument']}\n")
-print(f"Bug \n{programming_words['Bug']}\n")
+for programming_word, meaning in programming_words.items():
+   print(f"{programming_word} \n{meaning}\n")
