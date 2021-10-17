baa40f04a91f730f6bd2e08823a9c5bedb9f0b5d
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-10-17 02:53:06 -0300

    Favorite number exercise implementation

---
+++ Exercícios/Capitulo 6/04 - favorite_number.py
@@ -0,0 +1,10 @@
+favorite_numbers = {
+    'Mike': 2,
+    'John': 12,
+    'Sarah': 1,
+    'Adam': 3,
+    'Andre': 5,
+}
+
+for key in favorite_numbers:
+    print(f"{key} favorite number is {favorite_numbers[key]}")
