d0f40d4d5d79dc1cac1e07e061f0999b8c9a9d7c
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-11-12 16:58:15 -0300

    Nesting lists inside a dictionary

---
+++ Exercícios/Capitulo 6/11 - favorite_languagesl.py
@@ -0,0 +1,15 @@
+favorite_languages = {
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phill': ['python', 'haskell'],
+}
+
+for name, languages in favorite_languages.items():
+    if len(languages) > 1:
+        print(f"\n{name.title()}'s favorite languages are:")
+    else:
+        print(f"\n{name.title()}'s favorite language is:")
+
+    for language in languages:
+        print(f"\t{language.title()}")
